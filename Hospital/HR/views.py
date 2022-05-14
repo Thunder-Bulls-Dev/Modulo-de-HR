ae3613e822f5294ff96fbea70849593995c096fc
Sergio Samano <sergio.checo09.ss@gmail.com>
2022-05-13 19:46:16 -0500

    7:45 p.m. 13/05/2022 Samano

--- Hospital/HR/views.py
+++ Hospital/HR/views.py
@@ -1,5 +1,37 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
+from django.template import loader
+
+from .models import Empleado
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the HR index.")
+    latest_empleado_list = Empleado.objects.order_by('-curp')[:5]
+    template = loader.get_template('HR/index.html')
+    context = {
+        'latest_empleado_list': latest_empleado_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, empleado_id):
+    empleado_id = get_object_or_404(Empleado, pk=empleado_id_id)
+    return render(request, 'HR/detail.html', {'empleado_id': empleado_id})
+
+
+def results(request, empleado_id):
+    response = "You're looking at the results of empleado %s."
+    return HttpResponse(response % empleado_id)
+
+
+def vote(request, empleado_id):
+    return HttpResponse("You're voting on empleado %s." % empleado_id)
+
+
+def detail(request, empleado_id):
+    try:
+        empleado = Empleado.objects.get(pk=empleado_id)
+    except Empleado.DoesNotExist:
+        raise Http404("Empleado does not exist")
+    return render(request, 'HR/detail.html', { empleado: empleado})
